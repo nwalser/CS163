b73bbad64dd8ad88d801f2daffb5d6a0675a454c
Nathaniel Walser <personal@nathaniel-walser.com>
2025-10-28 22:50:28 -0700

    updated visualization to have correct position

--- webapp/my-app/app/page.tsx
+++ webapp/my-app/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen bg-gray-900">
       <ThreeEarthWithNSIDCOverlay
         overlay={{
-          src: '/overlays/age3.png',
-          extentMeters: [-6850000, -5350000, 6450000, 5850000],
+          src: '/overlays/age5.png',
+          extentMeters: [-3850000, -5350000, 3750000, 5850000], // <- replace with yours
           projection: 'north',
           outWidth: 4096,
         }}
